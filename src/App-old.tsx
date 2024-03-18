49355d91af66717aa19cc86f9a5bbf98b4d2d241
Roger Dunham <roger.dunham@apryse.com>
2024-03-18 13:03:37 +1300

    Refreshing UI - remove commented code

--- src/App-old.tsx
+++ src/App-old.tsx
@@ -1,46 +1,29 @@
 import WebViewer from '@pdftron/webviewer';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import './App.css';
-
-const event = new Event("makeVisible");
 
 class TextContentPanel extends React.Component {
   state = { text: '' };
   // Called after the component is mounted, similar to the initial effect in useEffect
   componentDidMount() {
     this.loadText();
-         // @ts-ignore comment.
+    // @ts-ignore comment.
     this.props.documentViewer.addEventListener('pageNumberUpdated', async () => {
-      console.log('1234')
       this.loadText();
-             // @ts-ignore comment.
-      // const text = await this.props.getTextOnPage();
-      // const txt2 = "Wibble " + text;
-      // this.setState({txt2});
     }
     );
-       // @ts-ignore comment.
 
-    this.props.documentViewer.addEventListener('visibilitychange', async () => {
-      console.log('2234')
-      this.loadText();
-             // @ts-ignore comment.
-      // const text = await this.props.getTextOnPage();
-      // const txt2 = "Wibble " + text;
-      // this.setState({txt2});
-    }
-    );
   }
 
   componentWillUnmount() {
-         // @ts-ignore comment.
+    // @ts-ignore comment.
     this.props.documentViewer.removeEventListener('pageNumberUpdated', this.handlePageNumberUpdated);
   }
 
   loadText = async () => {
-         // @ts-ignore comment.
+    // @ts-ignore comment.
     const text = await this.props.getTextOnPage();
-    this.setState({text});
+    this.setState({ text });
   }
 
   render() {
@@ -56,7 +39,6 @@ class TextContentPanel extends React.Component {
 
 function App() {
   const viewerDiv = useRef<HTMLDivElement>(null);
-  const [pnlCustom, setPanelCustom]=useState<any>();
   useEffect(() => {
     WebViewer({
       path: 'lib',
@@ -66,7 +48,7 @@ function App() {
       const { UI, Core } = instance;
       const { documentViewer } = instance.Core;
 
-      
+
       UI.enableFeatures([UI.Feature.FilePicker]);
 
       // @ts-ignore comment.
@@ -81,34 +63,27 @@ function App() {
           const text = await doc.getTextByPageAndRect(currentPageNum, rect);
           return text;
         }
-        //if no document is loaded then return an empty string 
-       // return "Z";
+        //if no document is loaded then return something to help debugging
+        return "No data";
       }
 
-   
+
       const createPanel = () => {
         //Add a new panel that contains the text                 
-        const pnl = {
-          dataElement: 'customPanel',
-          location: 'right',
-          // @ts-ignore comment.
-          //render: () => <div>getTextOnPage</div>
-            render: () => <TextContentPanel getTextOnPage={getTextOnPage} documentViewer={documentViewer} />,
-        };
-         // @ts-ignore comment.
+        // @ts-ignore comment.
         instance.UI.addPanel(
-           // @ts-ignore comment.
-          pnl);
-          setPanelCustom(pnl);
-          console.log('settign pnlCustom');
-          console.log(pnl);
-          console.log({getTextOnPage})
+          {
+            dataElement: 'customPanel',
+            location: 'right',
+            // @ts-ignore comment.
+            render: () => <TextContentPanel getTextOnPage={getTextOnPage} documentViewer={documentViewer} />,
+          });
       }
 
       documentViewer.addEventListener('documentLoaded', () => {
-      createPanel();
+        createPanel();
       });
-     // instance.UI.openElements(['customPanel']); 
+
       // @ts-ignore comment.
       const myStatefulButton = new instance.UI.Components.StatefulButton({
         initialState: 'Show',
@@ -120,38 +95,8 @@ function App() {
               update('Hide');
               const doc = documentViewer.getDocument();
               if (doc) {
-                // //Get and store the text for the current page
-                // const currentPageNum = documentViewer.getCurrentPage();
-                // const info = doc.getPageInfo(currentPageNum);
-                // const rect = new Core.Math.Rect(0, 0, info.width, info.height)
-                // const txt = await doc.getTextByPageAndRect(currentPageNum, rect);
-
-                //Add a new panel that contains the text                
-                // instance.UI.addPanel({
-                //   dataElement: 'customPanel',
-                //   location: 'left',
-
-                //   // @ts-ignore comment.
-                //  render:()=>{
-                //   return <div className='custom-panel'>
-                //     {txt}
-                //   </div>
-                //  }
-                // })
-
                 //Make the panel visible
                 instance.UI.openElements(['customPanel']);
-                // if ({pnlCustom})
-                // {
-                //   const p = {pnlCustom}
-                //   console.log("pnlCustom is NOT null")
-                //   console.log(p)
-                //    // @ts-ignore comment.
-                // p.dispatchEvent(event);
-                // }
-                // else{
-                //   console.log("pnlCustom is null")
-                // }
               }
             },
             title: 'Show',
@@ -161,16 +106,6 @@ function App() {
             onClick: (update: any) => {
               update('Show');
               instance.UI.closeElements(['customPanel']);
-                            
-              // // @ts-ignore comment.
-              // const panelsList= instance.UI.getPanels();
-
-              // // Remove the created panel - alternatively you could write code to reuse the panel and update it.
-              // // @ts-ignore comment.
-              // const newList = panelsList.filter(element=> element._dataElement!='customPanel')
-
-              // // @ts-ignore comment.
-              // instance.UI.setPanels(newList);
             },
             title: 'Close',
           },
@@ -190,7 +125,7 @@ function App() {
         grow: 0,
         gap: 12,
         position: 'end',
-        setJustifyContent:('center'),
+        setJustifyContent: ('center'),
         stroke: true,
         dimension: {
           paddingTop: 8,
@@ -206,12 +141,9 @@ function App() {
       });
       // @ts-ignore comment.
       instance.UI.setModularHeaders([topHeader]);
-    }
-
-    );
-
+    });
   }
-  , [])
+    , [])
   return (
     <>
       <div className='webviewer' ref={viewerDiv}></div>
